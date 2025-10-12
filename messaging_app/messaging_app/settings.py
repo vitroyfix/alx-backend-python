675d3b1e96860604d8a0470d759f4b0903d71a44
Peter Wangimwa <peterwangimwa@gmail.com>
2025-10-12 19:00:14 +0300

    configured db settings to use environment variables

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,15 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME', os.environ.get('MYSQL_DATABASE', 'messaging_db')),
+        'USER': os.environ.get('DATABASE_USER', os.environ.get('MYSQL_USER', 'messaging_user')),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', os.environ.get('MYSQL_PASSWORD', 'password')),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
